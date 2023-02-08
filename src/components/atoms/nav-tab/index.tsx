a88dec536ebd4aab5200ffd98ba1f7167e483065
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-02-08 11:31:10 -0300

    style: :lipstick: updated Tab (Navbar) style
    Now they have a smooth transition, and the text will turn to Italic when Tab is activated.

--- src/components/atoms/nav-tab/index.tsx
+++ src/components/atoms/nav-tab/index.tsx
@@ -16,6 +16,6 @@ export default function NavTab({ href, title }: any) {
 }
 
 const Tab = tw(Link) <{ children: React.ReactNode, href: string, active: boolean }>`
-  flex h-full w-40 content-center items-center justify-center border border-l-0  border-zinc-600 text-center hover:bg-neutral-700 hover:text-zinc-300
-  ${(p) => (p.active ? "border-b-0 border-t-red-400 bg-zinc-800 text-zinc-200 shadow-[0_-3px_4px_-2px_rgba(255,96,89,0.4)]" : "")}
+flex h-full content-center items-center justify-center border border-l-0 border-zinc-600 px-10 text-center transition duration-300 hover:bg-neutral-700 hover:text-zinc-300
+  ${(p) => (p.active ? "italic border-b-0 border-t-red-400 bg-zinc-800 text-zinc-200 shadow-[0_-3px_4px_-2px_rgba(255,96,89,0.4)]" : "")}
 `
