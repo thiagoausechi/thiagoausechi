599c56c402ef51ca8af62ec24798dee85306ffc8
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-04-27 11:27:47 -0300

    chore: :wrench: Added media queries for Tablet and Desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      fontFamily:
-      {
-        "fira": "var(--font-fira)",
-        "east": "var(--font-east-sea)"
-      }
+      screens: {
+        tablet: '425px',
+        // => @media (min-width: 425px) { ... }
+
+        desktop: '1360px',
+        // => @media (min-width: 1360px) { ... }
+      },
     },
   },
   plugins: [],
