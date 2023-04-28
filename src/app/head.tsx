e708ed8fcacad0ceaf534fdfc5ac7b66504ba2b8
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-04-28 12:18:33 -0300

    feat: :bento: Added Favicon

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,10 @@
+import FaviconMeta from '../components/atoms/favicon-meta'
+
 export default function Head() {
   return (
     <>
       <title>thiago ausechi | portfolio</title>
+      <FaviconMeta />
       <meta content='width=device-width, initial-scale=1' name='viewport' />
       <link rel='icon' href='/favicon.ico' />
     </>
