e3c4467e0d34e62f3d0d67b70e07f93bb03abedb
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-07-07 09:40:59 -0300

    fix: :sparkles: Updated head metadata to Next13

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,39 @@
+import { east, fira, virgil } from '@/assets/fonts'
 import Header from '@/layout/header'
 import '@/styles/globals.css'
+import type { Metadata } from 'next'
 import tw from 'tailwind-styled-components'
-import { east, fira, virgil } from '../assets/fonts'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'thiago ausechi | portfolio',
+  description: '',
+  applicationName: 'Thiago Ausechi Portfolio',
+  category: 'portfolio',
+  keywords: [
+    'Thiago',
+    'Ausechi',
+    'Front End',
+    'Front-end',
+    'Designer',
+    'Dev',
+    'Developer',
+    'Next.js',
+    'React',
+    'TypeScript',
+  ],
+  colorScheme: 'dark',
+  themeColor: '#27272a',
+  creator: 'Thiago Ausechi',
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+      'pt-BR': '/pt-BR',
+    },
+  },
+  icons: {
+    shortcut: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({
