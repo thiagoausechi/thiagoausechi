6e4b7e76dd23e3133b45417560b7243c57ceb677
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-07-07 10:41:16 -0300

    feat: :iphone: Mobile won't load Desktop Home Page
    Mobile devices usually have a data transmission limit, so it makes no sense to load the desktop version of the Home Page since it will never be shown.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import HomePageContainer from '@/containers/home-page'
 import LinktreeContainer from '@/containers/linktree-page'
+import { headers } from 'next/headers'
+import { getSelectorsByUserAgent } from 'react-device-detect'
 
-// TODO: Add a Dynamic loadin\g for Desktop Version
 export default function HomePage() {
+  const userAgent = headers().get('user-agent')
+  let isMobile = false
+  if (userAgent) isMobile = getSelectorsByUserAgent(userAgent).isMobile
+
   return (
     <>
       <LinktreeContainer />
-      <HomePageContainer />
+      {!isMobile && <HomePageContainer />}
     </>
   )
 }
